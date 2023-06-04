aeee6615051a77faf1becf0b1c7efd9de0f8424c
Tishida <archersparadox.100@gmail.com>
2023-06-04 16:13:53 +0900

    controller

--- src/main/kotlin/com/example/sample_qiita/controller/QiitaController.kt
+++ src/main/kotlin/com/example/sample_qiita/controller/QiitaController.kt
@@ -3,18 +3,23 @@ package com.example.sample_qiita.controller
 import com.example.sample_qiita.model.Qiita
 import com.example.sample_qiita.repository.QiitaRepository
 import jakarta.validation.Valid
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.client.RestTemplate
 
 
 @RestController
 @RequestMapping("/api")
 class QiitaController(private val qiitaRepository: QiitaRepository) {
 
+    private val restTemplate = RestTemplate()
+
     @GetMapping("/qiitas")
-    fun getAllQiitas(): List<Qiita> =
-        qiitaRepository.findAll()
+    fun getAllQiitas(): List<Qiita> {
+        return qiitaRepository.findAll()
+    }
 
     @PostMapping("/qiitas")
     fun createNewQiita(@Valid @RequestBody qiita: Qiita): Qiita =
@@ -22,14 +27,27 @@ class QiitaController(private val qiitaRepository: QiitaRepository) {
 
     @GetMapping("/qiitas/{id}")
     fun getQiitaBiId(@PathVariable(value = "id") qiitaed: Long): ResponseEntity<Qiita> {
-        return qiitaRepository.findById(qiitaed).map { qiita ->
-            ResponseEntity.ok(qiita)
-        }.orElse(ResponseEntity.notFound().build())
+        println("IOIOIOIOIOOOIOIOIOIOOOI")
+
+
+
+        return ResponseEntity.ok(
+            Qiita(
+                id = 1,
+                title = "ダミータイトル",
+                content = "ダミー内容"
+            )
+        )
+//        return qiitaRepository.findById(qiitaed).map { qiita ->
+//            ResponseEntity.ok(qiita)
+//        }.orElse(ResponseEntity.notFound().build())
     }
 
     @PutMapping("/qiitas/{id}")
-    fun updateQiitaById(@PathVariable(value = "id") qiitaed: Long,
-                        @Valid @RequestBody newQiita: Qiita): ResponseEntity<Qiita> {
+    fun updateQiitaById(
+        @PathVariable(value = "id") qiitaed: Long,
+        @Valid @RequestBody newQiita: Qiita
+    ): ResponseEntity<Qiita> {
 
         return qiitaRepository.findById(qiitaed).map { existingQiita ->
             val updateQiita: Qiita = existingQiita
@@ -46,5 +64,29 @@ class QiitaController(private val qiitaRepository: QiitaRepository) {
             qiitaRepository.delete(qiita)
             ResponseEntity<Void>(HttpStatus.OK)
         }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @GetMapping("/external/events")
+    fun getTokyoEvents(): ResponseEntity<Void> {
+        println("events")
+
+        fun fetchDataFromExternalApi(): String {
+            val apiUrl = "https://catalog.data.metro.tokyo.lg.jp/dataset/t000003d0000000027" // 外部APIのエンドポイントURL
+
+            // GETリクエストを送信してデータを取得
+            val response: ResponseEntity<String> =
+                restTemplate.exchange(apiUrl, HttpMethod.GET, null, String::class.java)
+
+            if (response.statusCode.is2xxSuccessful) {
+                println("response.body")
+                println(response.body)
+                return response.body ?: ""
+            } else {
+                throw RuntimeException("Failed to fetch data from the external API")
+            }
+        }
+
+        fetchDataFromExternalApi()
+        return TODO("Provide the return value")
     }
 }
